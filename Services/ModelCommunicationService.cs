17e61a93a215f6cd09f7d4ee8366c972a5284333
Mohamadreza Nakhleh <neo.vortex@pm.me>
2024-02-16 11:52:08 +0000

    (clean) code cleanup

--- Services/ModelCommunicationService.cs
+++ Services/ModelCommunicationService.cs
@@ -12,31 +12,20 @@ public class ModelCommunicationService ( IMongoClient  _client , IHttpClientFact
      {
          var client = _httpClientFactory.CreateClient();
          
-         // Set authorization header
          client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "1c625a5a-91a1-49c1-ab33-9cf131d06dd5");
- 
-         // Serialize input object to JSON
-         var jsonPayload = System.Text.Json.JsonSerializer.Serialize(input);
- 
-         Console.WriteLine(jsonPayload);
- 
-         // Send POST request with authorization header
          var result = await client.PostAsJsonAsync(Consts.Consts.MODEL_URL + symbol, input);
  
          if (!result.IsSuccessStatusCode)
          {
              throw new Exception("Model server error");
          }
- 
-         // Read response content
+         
          var response = await result.Content.ReadAsStringAsync();
- 
-         // Deserialize response JSON to ModelOutput object
+         
          return  System.Text.Json.JsonSerializer.Deserialize<ModelOutput>(response);
      }
      catch (Exception ex)
      {
-         // Handle exceptions
          Console.WriteLine($"An error occurred: {ex.Message}");
          return null;
      }
